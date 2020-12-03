88d0ee70f67db19f94df83e0817731445307e908
CeliaNoriega <noriegacelia2124@gmail.com>
2020-12-03 14:59:59 -0800

    restructuring

--- Develop/controllers/apiRoutes.js
+++ Develop/controllers/apiRoutes.js
@@ -1,12 +1,30 @@
-const router = require('express').Router
-
+const router = require('express').Router()
+const { join } = require('path')
 const db = require('../db/db.json')
 
-router.get("/api/notes", (req, res) => {
-  res.json('')
+router.get("/notes", (req, res) => {
+  fs.readFile(join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
+    if (err) { console.log(err) }
+
+    let notes = JSON.parse(data)
+
+    res.json(notes)
+  })
 })
 
+router.post('/notes', (req, res) => {
+  if (err) { console.log(err) }
 
+  let notes = JSON.parse(data)
+
+  notes.push(req.body)
+
+  fs.writeFile(join(__dirname, '..', 'db', 'db.json'), 'utf8' ))
+})
+
+router.delete('/notes/:id', (req, res) => {
+  
+})
 
 
 
--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,3 +1,3 @@
 const router = require('express').Router;
 
-router.usearguments()
+router.use('/', require)
