d9f63af6a85bb74805a2c61e95f2ebaacd6ac2c7
CeliaNoriega <noriegacelia2124@gmail.com>
2020-12-07 22:26:42 -0800

    re-ordered files

--- Develop/controllers/viewRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-
-router.get('/notes', (req, res) => {
-  res.sendFile(join(__dirname, '..', 'public', 'notes.html'))
-}) 
-
-router.ge('/*', (req, res) => {
-  res.sendFile(join(__dirname, '..', 'public', 'notes.html')) )
